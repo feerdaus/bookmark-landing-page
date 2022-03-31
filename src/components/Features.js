8b1cdab7fd23be56339ca28459572517549e5a72
feerdaus <firdauslawal@gmail.com>
2022-03-31 13:35:05 +0100

    Add and Styled Features Section

---
+++ src/components/Features.js
@@ -0,0 +1,95 @@
+import React, { useState } from "react";
+import "./features.css";
+import pic1 from "./images/slide1.png";
+import pic2 from "./images/slide2.png";
+import pic3 from "./images/slide3.png";
+
+const slides = [
+  {
+    image: pic1,
+    heading: "Bookmark in one click",
+    content:
+      "Organize your bookmarks however you like. Our simple drag-and-drop interface gives you complete control over how you manage your favourite sites.",
+  },
+  {
+    image: pic2,
+    heading: "Intelligent Search",
+    content:
+      "Our powerful search feature will help you find saved sites in no time at all. No need to trawl through all of your bookmarks.",
+  },
+  {
+    image: pic3,
+    heading: "Bookmark in one click",
+    content:
+      "Easily share your bookmarks and collections with others. Create a shareable link that you can send at the click of a button.",
+  },
+];
+
+const Features = () => {
+  const [panel, setPanel] = useState(0);
+
+  return (
+    <section className="py-12">
+      <div className="container mx-auto">
+        <div className="text-center px-4 title mx-auto">
+          <h2 className="mb-4">Features</h2>
+          <p>
+            Our aim is to make it quick and easy for you to access your
+            favourite websites. Your bookmarks sync between your devices so you
+            can access them on the go.
+          </p>
+          <div className="features-links md:justify-around my-12 flex flex-col md:flex-row">
+            <p
+              className={panel === 0 ? "link" : ""}
+              onClick={() => setPanel(0)}
+            >
+              Simple Bookmarking
+            </p>
+            <p
+              className={panel === 1 ? "link" : ""}
+              onClick={() => setPanel(1)}
+            >
+              Speedy Searching
+            </p>
+            <p
+              className={panel === 2 ? "link" : ""}
+              onClick={() => setPanel(2)}
+            >
+              Easy Sharing
+            </p>
+          </div>
+        </div>
+
+        <div className="flex flex-col md:flex-row">
+          {slides.map((slide, index) => {
+            return (
+              <div
+                className={`md:relative pr-4 ${
+                  panel === index ? "block" : "hidden"
+                }`}
+              >
+                <div>
+                  <img
+                    src={slide.image}
+                    alt="features"
+                    width={600}
+                    height={400}
+                  />
+                </div>
+                <div className="md:absolute bookmark text-center mx-auto md:text-left mt-16 md:mt-0">
+                  <h2>{slide.heading}</h2>
+                  <p className="my-4">{slide.content}</p>
+                  <button className=" info-btn rounded py-4 px-6">
+                    More info
+                  </button>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Features;
